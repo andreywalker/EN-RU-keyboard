fa498b74210410dc5424a98cab925839b78062ea
Andrey <98677791+andreywalker@users.noreply.github.com>
2024-07-11 15:42:26 +0500

    test

--- INSTALL2.bat
+++ INSTALL2.bat
@@ -1,4 +1,19 @@
 @echo off
 python get-pip.py 
+
+REM Get the current Python Scripts directory
+for /f "usebackq tokens=*" %%a in (`python -c "import sysconfig; print(sysconfig.get_path('scripts'))"`) do (
+    set "pip_path=%%a"
+)
+
+REM Add pip directory to system PATH if it exists
+if exist "%pip_path%" (
+    echo Adding %pip_path% to PATH...
+    setx PATH "%pip_path%;%PATH%" /M
+    echo Added %pip_path% to PATH successfully.
+) else (
+    echo Error: pip directory not found.
+)
+
 pip install pyperclip
 python py_to_ps.py > nul 2>&1
